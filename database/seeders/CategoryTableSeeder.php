9fa52a6f87de754e11fef888a51daca7e5770f3a
Oleksha <deryabino@gmail.com>
2025-10-17 15:48:10 +0400

    Часть 19. Модуль «Категории» (I) | Создание таблицы и начальное заполнение категорий

---
+++ database/seeders/CategoryTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['parent_id' => null, 'name' => 'Одежда', 'url' => 'clothing'],
+            ['parent_id' => null, 'name' => 'Электроника', 'url' => 'electronics'],
+            ['parent_id' => null, 'name' => 'Бытовая техника', 'url' => 'appliances'],
+            ['parent_id' => 1, 'name' => 'Мужская', 'url' => 'men'],
+            ['parent_id' => 1, 'name' => 'Женская', 'url' => 'women'],
+            ['parent_id' => 1, 'name' => 'Детская', 'url' => 'kids'],
+        ];
+        foreach ($categories as $data) {
+            Category::create([
+                'parent_id' => $data['parent_id'],
+                'name' => $data['name'],
+                'url' => $data['url'],
+                'image' => '',
+                'size_chart' => '',
+                'discount' => 0,
+                'description' => '',
+                'meta_title' => '',
+                'meta_description' => '',
+                'meta_keywords' => '',
+                'status' => 1,
+                'menu_status' => 1,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
